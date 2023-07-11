74790a2ef390dc4c057e3918504397ed1065ad82
weibin yu <wy222ad@student.lnu.se>
2023-07-11 22:40:01 +0200

    cleaner approach

--- BinarySearch.c
+++ BinarySearch.c
@@ -5,39 +5,28 @@
 
 int main(){
     int arrLenght = 10;
-    int search_val = 0;
+    int search_val = 19;
     
     int *res = genSortedArr(arrLenght);
     for (int i = 0; i < arrLenght; i++) {
         printf("%d ",*(res + i));
     }
-    int pivot = arrLenght/2;
-    int current_end = arrLenght;
-    int count = 0;
+    int low = 0;
+    int high = arrLenght;
     printf("\n");
-    while (0 <= pivot && pivot < arrLenght) {
+    while (low < high) {
+      int pivot = low + (high-low)/2;
       int current = res[pivot];
       printf("start p %d \n",pivot);
       printf("current %d \n",current);
       if ( current == search_val) {
+        printf("found");
         return pivot; 
       }else if ( current > search_val) { 
-        if (pivot == 0) {
-            printf("not found");
-            return -1;        
-        }
-        current_end = pivot;
-        pivot = current_end/2;     
+        high = pivot;
       } else if( current < search_val) {
-        int inc = (current_end - pivot)/2;
-        if(inc > 0)
-          pivot = pivot + inc;     
-        else
-          pivot = pivot + 1;
+        low = pivot + 1;
       }   
-      printf("end p %d \n",pivot);
-      count ++;
-      if(count == 5) return 0;
     }
     free(res);
     printf("not found");
